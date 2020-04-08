64740b4f10a9faee4da0af281e14756e48280a75
cathymnguyen3 <59972390+cathymnguyen3@users.noreply.github.com>
2020-04-08 15:30:32 -0400

    Add files via upload

--- Screens/FacilitiesAddresses.js
+++ Screens/FacilitiesAddresses.js
@@ -1,60 +1,72 @@
-/*
-Api call for Facilities - Address
-04/01/2020
+//Screen2:  api call for facilities - building addresses
+// 04/04/2020
 
-*/
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native';
+import {
+  useState,
+  useEffect
+} from 'react';
 
-var api_link = `https://api.devhub.virginia.edu/v1/facilities/categories`;
-
+var api_link = `https://api.devhub.virginia.edu/v1/facilities`;
 export default function FacilitiesAddresses(props) {
-    // Use hooks to store functional component state
-    const [busState, setBusState] = useState({
-        loaded: false,
-        busses: []
+  // Use hooks to store functional component state
+  //fields/attributes
+  const [buildingsState, setBuildingsState] = useState({
+    loaded: false,
+    buildings: []
+  });
+  // Function to get/update our stored bus data
+  const updateBuildings = async () => {
+    let data = await fetch(api_link).then(res => res.json());
+    setBuildingsState({
+      loaded: true,
+      buildings: data
     });
+  }
+  // Trigger the initial loading of the building data
+  useEffect(() => {
+    updateBuildings();
+  }, []);
 
-    // Function to get/update our stored bus data
-    const updateBusses = async () => {
-        let data = await fetch(api_link).then(res => res.json());
-        setBusState({
-            loaded: true,
-            busses: data.vehicles
-        });
-    }
-
-    // Trigger the initial loading of the bus data
-    useEffect(() => {
-        updateBusses();
-    }, []);
-
-    if(!busState.loaded) return (<Text>Loading...</Text>);
-    return (
-        <View>
-            <TouchableOpacity   style={styles.reloadBtn}
-                                onPress={() => updateBusses()}>
-                <Text style={styles.reloadTxt}>Refresh Data</Text>
-            </TouchableOpacity>
-            <View>
+  if (!buildingsState.loaded) return ( < Text > Loading... < /Text>);
+        return ( <
+            View>
+            <TouchableOpacity style = {
+              styles.reloadBtn
+            }
+            onPress = {
+              () => updateBuildings()
+            } >
+            <Text style = {
+              styles.reloadTxt
+            }> Refresh Data </Text>
+             </TouchableOpacity>
+              <View>
                 {
-                    busState.busses.map(bus => (<Text key={bus.Name}>{`Name: ${bus.Name} | Address: ${bus.Address}`}</Text>)))
+                  buildingsState.buildings.map(buildings => (
+                    <Text key = { buildings.Name }>{ `Name: ${buildings.Name} | Address: ${buildings.Address}`}</Text>
+                  ))
                 }
+              </View>
             </View>
-        </View>
-    )
-}
+              )
+            }
 
-const styles = StyleSheet.create({
-    reloadBtn: {
-        margin: 5,
-        flexDirection: "row",
-        justifyContent: "center"
-    },
-    reloadTxt: {
-        padding: 5,
-        backgroundColor: "#232dfb",
-        color: "white",
-    }
-})
+            const styles = StyleSheet.create({
+              reloadBtn: {
+                margin: 5,
+                flexDirection: "row",
+                justifyContent: "center"
+              },
+              reloadTxt: {
+                padding: 5,
+                backgroundColor: "#232dfb",
+                color: "white",
+              }
+            })
